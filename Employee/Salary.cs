41c595beabf1ba36e596c4d17dcd47dce50ef438
COMPUTER\user <nihelfou@gmail.com>
2018-10-11 12:12:18 +0700

    no message

---
+++ Employee/Salary.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employee
+{
+    class Salary : IAccountantSalary, IEmployeeSalary, ISizeSalary
+    {
+        public int SalaryAmount { get; private set; }
+        public int SizeSalary { get; set; }
+
+        public void Pay(int amount)
+        {
+            SalaryAmount += amount;
+        }
+    }
+}
